ad4af29cdf96cda76fbd8ed1d8b64ea71e2c0a3d
ALE-K <ALE-K@PC>
2020-09-30 10:27:39 +0100

    Project created

--- src/main/java/Kata1.java
+++ src/main/java/Kata1.java
@@ -1,10 +1,10 @@
 
 import java.time.LocalDate;
-import static java.util.Calendar.MONTH;
+import java.time.Month;
 
 public class Kata1 {
     public static void main(String[] args) {
-        Person person = new Person("Luis Hernandez", LocalDate.of(1934, MONTH.MARCH, 31));
+        Person person = new Person("Luis Hernandez", LocalDate.of(1934, Month.AUGUST, 12));
         System.out.println(person.getName() + "=" + person.getYears());
 }
 }
